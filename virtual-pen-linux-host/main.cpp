013ffd92fd3a2f18894ee937acacea64f0122a37
Mathieu Beliveau <mathieu.beliveau.1@gmail.com>
2024-04-14 03:06:33 -0400

    fix icon

--- virtual-pen-linux-host/main.cpp
+++ virtual-pen-linux-host/main.cpp
@@ -4,7 +4,6 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setWindowIcon(QIcon("./images/virtual-pen.svg"));
     foreach (QString arg, a.arguments()) {
         if(arg == "-d"){
             MainWindow::isDebugMode = true;
