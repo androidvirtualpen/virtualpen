512351b2c351734c21343486f37682e505a9c3a9
Mathieu Beliveau <mathieu.beliveau.1@gmail.com>
2024-04-17 21:37:51 -0400

    fix flatpak issue upon first connection with the device

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 rm -rf ./build-dir
 rm -rf ./export
+flatpak uninstall --noninteractive org.flatpak.virtualpen
 flatpak-builder --disable-cache --force-clean build-dir ./org.flatpak.virtualpen.json
 flatpak build-export export build-dir
 flatpak build-bundle export virtualpen.flatpak org.flatpak.virtualpen --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo 
