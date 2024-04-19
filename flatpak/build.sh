d7552a87cb7af6b795c5c3c125ebdc496c344431
Mathieu Beliveau <mathieu.beliveau.1@gmail.com>
2024-04-19 00:16:54 -0400

    tidy up flatpak

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -1,8 +1,11 @@
 #! /bin/sh
-rm -rf ./build-dir
-rm -rf ./export
-flatpak uninstall --noninteractive org.flatpak.virtualpen
-flatpak-builder --disable-cache --force-clean build-dir ./org.flatpak.virtualpen.json
-flatpak build-export export build-dir
-flatpak build-bundle export virtualpen.flatpak org.flatpak.virtualpen --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo 
-flatpak-builder --install --force-clean build-dir ./org.flatpak.virtualpen.json
+flatpak run org.flatpak.Builder --force-clean --sandbox --user --install --ccache --mirror-screenshots-url=https://dl.flathub.org/media/ --repo=repo builddir ./org.flatpak.virtualpen.json
+ostree commit --repo=repo --canonical-permissions --branch=screenshots/x86_64 builddir/files/share/app-info/media
+echo "Done building \n\n"
+echo "Running linter for repo \n"
+flatpak run --command=flatpak-builder-lint org.flatpak.Builder repo repo
+echo "Running linter for manifest \n"
+flatpak run --command=flatpak-builder-lint org.flatpak.Builder manifest org.flatpak.virtualpen.json
+echo "Generating standalone bundle \n"
+flatpak build-export export builddir
+flatpak build-bundle export virtualpen.flatpak org.flatpak.virtualpen --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
