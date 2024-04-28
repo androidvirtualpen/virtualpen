9bcefc04a949b34d126fec806688a19eb69fa9b6
Mathieu Beliveau <mathieu.beliveau.1@gmail.com>
2024-04-28 18:45:58 -0400

    remove flatpak dir (moved to its own repo)

--- flatpak/setup.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-groupadd -r virtualpen
-usermod -aG virtualpen $USER
-echo 'SUBSYSTEM=="usb", MODE="0777", GROUP="plugdev"\nKERNEL=="uinput", MODE="0777", GROUP="virtualpen", OPTIONS+="static_node=uinput"' | sudo tee /etc/udev/rules.d/60-android-virtual-pen.rules
-udevadm control --reload
-udevadm trigger
